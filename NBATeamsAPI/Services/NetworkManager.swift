1d02e924cef159f83167bf46a72f62898e44b7f6
balandzin <balandzin2602@gmail.com>
2024-02-05 18:29:56 +0300

    start coding

--- NBATeamsAPI/Services/NetworkManager.swift
+++ NBATeamsAPI/Services/NetworkManager.swift
@@ -39,3 +39,41 @@ final class NetworkManager {
         }.resume()
     }
 }
+
+
+
+//import Foundation
+//
+//enum NetworkError: Error {
+//    case noData
+//    case decodingError
+//}
+//
+//final class NetworkManager {
+//    static let shared = NetworkManager()
+//    
+//    private init() {}
+//    
+//    func fetch<T: Decodable>(_ type: T.Type, from url: URL, completion: @escaping(Result<T, NetworkError>) -> Void) {
+//        URLSession.shared.dataTask(with: url) { data, _, error in
+//            guard let data else {
+//                print(error ?? "No error description")
+//                return
+//            }
+//            
+//            do {
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .convertFromSnakeCase
+//                
+//                let dataModel = try decoder.decode(T.self, from: data)
+//                DispatchQueue.main.async {
+//                    completion(.success(dataModel))
+//                }
+//            } catch {
+//                completion(.failure(.decodingError))
+//            }
+//            
+//        }.resume()
+//    }
+//}
+
--- NBATeamsAPI/ViewControllers/GamesViewController.swift
+++ NBATeamsAPI/ViewControllers/GamesViewController.swift
@@ -65,3 +65,63 @@ extension GamesViewController {
         }
     }
 }
+
+
+//final class GamesViewController: UITableViewController {
+//    
+//    /*
+//    API (сведения об играх команд NBA)
+//    находится по адресу https://www.balldontlie.io/home.html#introduction
+//     */
+//    
+//    private var games: Games!
+//    private var gameData: [Game] = []
+//    private let networkManager = NetworkManager.shared
+//    private let url = URL(string: "https://www.balldontlie.io/api/v1/games")
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.rowHeight = 100
+//        fetchGames()
+//    }
+//    
+//// MARK: - Table view data source
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        gameData.count
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "gameCell", for: indexPath) as! GameCell
+//        let game = gameData[indexPath.row]
+//        cell.configure(with: game)
+//        return cell
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+//    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+//        let detailsVC = segue.destination as? GameDetailsViewController
+//        detailsVC?.game = gameData[indexPath.row]
+//    }
+//}
+//
+//// MARK: - Networking
+//extension GamesViewController {
+//    
+//    private func fetchGames() {
+//        networkManager.fetch(Games.self, from: url!) { [weak self] result in
+//            guard let self else { return }
+//            switch result {
+//            case .success(let games):
+//                self.games = games
+//                gameData = games.data
+//                tableView.reloadData()
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//    }
+//}
